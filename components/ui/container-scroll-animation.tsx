4112622d2d7b5547732a163377985be965453cc8
shorya1wd <shoryabhushan0@gmail.com>
2026-07-04 13:24:45 +0530

    fix scrolling container mobile length

--- components/ui/container-scroll-animation.tsx
+++ components/ui/container-scroll-animation.tsx
@@ -28,13 +28,13 @@ export const ContainerScroll = ({
 
   const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
   const scaleDesktop = useTransform(scrollYProgress, [0, 1], [1.05, 1]);
-  const scaleMobile = useTransform(scrollYProgress, [0, 1], [0.7, 0.9]);
+  const scaleMobile = useTransform(scrollYProgress, [0, 1], [0.85, 1]);
   const scale = isMobile ? scaleMobile : scaleDesktop;
   const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
 
   return (
     <div
-      className="h-240 md:h-280 flex items-start justify-center relative p-2 md:p-20 pt-10  md:pt-10"
+      className="h-[70rem] md:h-280 flex items-start justify-center relative p-2 md:p-20 pt-10 md:pt-10"
       ref={containerRef}
     >
       <div
@@ -83,7 +83,7 @@ export const Card = ({
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-[100rem] -mt-12 mx-auto min-h-120 md:min-h-160 w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      className="max-w-[100rem] -mt-12 mx-auto min-h-[36rem] md:min-h-160 w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
       <div className=" h-full w-full overflow-y-auto overflow-x-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 ">
         {children}
